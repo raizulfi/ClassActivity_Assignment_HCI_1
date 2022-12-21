ffe2ed2b9479a0055fb7208ef2e8027e6af2c86a
Raihan Zulfi <114371959+raizulfi@users.noreply.github.com>
2022-12-21 23:31:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Please put your answer here
+$('#signup-modal').hide()
+
+$(function() {
+
+    $('.signup-show').click(function(){
+      $('#signup-modal').fadeIn();
+    });
+
+    $('#close-modal').click(function(){
+      $('#signup-modal').fadeOut();
+    });
+
+});
